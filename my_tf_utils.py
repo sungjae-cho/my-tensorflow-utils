ce58c66e2ff34b6e765cb502240e78d19d556f6f
Sungjae Cho <sungjae.cho.1118@gmail.com>
2018-10-18 23:31:39 +0900

    Added print_nodes function.

--- my_tf_utils.py
+++ my_tf_utils.py
@@ -7,6 +7,14 @@ def print_node(node):
         sess.run(tf.global_variables_initializer())
         node_val = sess.run(node)
         print(node_val)
+        
+def print_nodes(nodes):
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as sess:
+        sess.run(tf.global_variables_initializer())
+        nodes_val = sess.run(nodes)
+        print(nodes_val)
 
 def print_shape(tensor):
     config = tf.ConfigProto()
